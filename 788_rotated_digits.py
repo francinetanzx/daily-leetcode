8882c87114e47d2bd5d3024de89074ab6de33e13
francinetanzx <francine.tanzx@gmail.com>
2026-05-03 22:28:12 +0800

    fix: fix and rename 788

--- 788_rotated_digits.py
+++ 788_rotated_digits.py
@@ -38,24 +38,24 @@ def rotatedDigits(n: int) -> int:
     Space: O(N)
     """
     valid_digits_mapping = {
-        0: 0,
-        1: 0, 
-        2: 1, 
+        0: 1,
+        1: 1, 
+        2: 2, 
         3: 0,
         4: 0, 
-        5: 1, 
-        6: 1,
+        5: 2, 
+        6: 2,
         7: 0,
-        8: 0,
-        9: 1
+        8: 1,
+        9: 2
     }
     count_of_good_numbers = 0
     tracker = [-1] * n
 
     for i in range(1, n + 1):
-        # if i < 10, there is no prefix to check, validate and move on
+        # if i < 10, there is no prefix to check, find category and move on
         if i < 10: 
-            count_of_good_numbers += 1 if valid_digits_mapping[i] else 0
+            count_of_good_numbers += 1 if valid_digits_mapping[i] == 2 else 0
             tracker[i-1] = valid_digits_mapping[i]
             continue 
 
@@ -63,17 +63,21 @@ def rotatedDigits(n: int) -> int:
         all_except_last_digit = i // 10 
         last_digit = i % 10
 
-        # check all_except_last_digit from tracker 
-        is_prefix_good = tracker[all_except_last_digit - 1]
-
         # check last_digit from valid_digits_mapping 
-        is_last_digit_good = valid_digits_mapping[last_digit]
+        last_digit_category = valid_digits_mapping[last_digit]
 
-        if is_prefix_good and is_last_digit_good: 
-            count_of_good_numbers += 1
+        # check all_except_last_digit from tracker 
+        prefix_category = tracker[all_except_last_digit - 1]
+
+        # if either category is an invalid rotation, update as invalid
+        if last_digit_category == 0 or prefix_category == 0: 
+            tracker[i-1] = 0
+        elif last_digit_category == 1 and prefix_category == 1:
             tracker[i-1] = 1
         else:
-            tracker[i-1] = 0
+            tracker[i-1] = 2
+            count_of_good_numbers += 1
     
     return count_of_good_numbers
 
+print(rotatedDigits(857))
